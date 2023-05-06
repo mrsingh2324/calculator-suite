14d5b865682e6ca2e24be9d4dca94d1bb355aaf3
satyam <satyam.12011172@gmail.com>
2023-05-06 14:47:38 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+      
+
+    </>
+  );
+}
+
+export default App;
